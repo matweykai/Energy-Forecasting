dbad1391dc10375c25427c70de4246974cd2da54
Matwey Ivanov <matveykai@list.ru>
2022-09-08 12:57:09 +0300

    Add get_exp_autoreg_prediction function
    Added function and updated research, where you can find explanation
    of model constantness

--- prediction_methods.py
+++ prediction_methods.py
@@ -1,4 +1,5 @@
 from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing
 from scipy import stats
 from scipy.special import inv_boxcox
 
@@ -18,5 +19,16 @@ def get_ARMA_prediction(data: list[float], p: int, d: int, q: int, forecast_len:
     # TODO: Add logging after prediction
     # Inverting box-cox transform
     result = inv_boxcox(pred, lmb)
+
+    return result
+
+
+def get_exp_autoreg_prediction(data: list[float], forecast_len: int) -> list[float]:
+    """Trains exponential autoregressive model and makes prediction for the specified forecast len"""
+    model = SimpleExpSmoothing(data)
+
+    model = model.fit(smoothing_level=0.6)
+
+    result = model.predict(len(data), len(data) + forecast_len)
 
     return result
